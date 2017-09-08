9097550b7703ae4a1ff5129a400832004f9071f4
erikzaksf <erik.zak.sf@gmail.com>
2017-09-08 15:45:11 -0700

    update method but still not able get funtionality with all three statments

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,14 +1,19 @@
+
 public def anagram(word1, word2)
   @word1 = word1
   @word2 = word2
+  word3 = @word1
   answer = ""
   word1.downcase!
   word2.downcase!
-  word1.delete! word2
-  if (word1.empty? == true)
-    return answer = "These are anagrams."
-  else
+  word3.delete! word2
+  if  (word1.reverse! == word2)
+   return answer = "These words are palindromes and anagrams."
+  elsif(word3.empty? == true)
+    answer = "These are anagrams."
+    return answer
+  else (word3.empty? == false)
     return answer = "These are not anagrams."
   end
-    return this.answer
+
 end
