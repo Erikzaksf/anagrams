c71b2de14535209997e1244d7c9b4c3db3ff9539
erikzaksf <erik.zak.sf@gmail.com>
2017-09-08 14:08:46 -0700

    refactor method to handle words with different cases

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,13 +1,14 @@
-
 public def anagram(word1, word2)
   @word1 = word1
   @word2 = word2
   answer = ""
+  word1.downcase!
+  word2.downcase!
   word1.delete! word2
   if (word1.empty? == true)
     return answer = "These are anagrams."
   else
     return answer = "These are not anagrams."
   end
-  return this.answer
+    return this.answer
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,4 +5,8 @@ describe('#anagram')do
   it ("return phrase this is an anagram when given two words containing the same letters")do
     expect(anagram("this","hits")).to(eq("These are anagrams."))
   end
+  it ("return phrase this is an anagram even when given two words with different typecase")do
+    expect(anagram("This","Hits")).to(eq("These are anagrams."))
+  end
 end
+  
